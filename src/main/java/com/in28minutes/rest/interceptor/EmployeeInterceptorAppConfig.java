79e81b8b2ce625620747a4b8f6093f0ae54f3979
MohaN@1234 <guru.mohanln@gmail.com>
2020-07-01 17:05:06 +0530

    first commit

---
+++ src/main/java/com/in28minutes/rest/interceptor/EmployeeInterceptorAppConfig.java
@@ -0,0 +1,20 @@
+package com.in28minutes.rest.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Component
+public class EmployeeInterceptorAppConfig extends WebMvcConfigurationSupport
+{
+	@Autowired
+	EmployeeInterceptor employeeInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry)
+	{
+		registry.addInterceptor(employeeInterceptor);
+	}
+
+}
